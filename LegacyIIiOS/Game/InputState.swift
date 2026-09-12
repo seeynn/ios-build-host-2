9cae323dd8548d16d3e248172e8d96c16a753c66
seeynn <z3envx@web.de>
2026-09-12 08:31:42 +0200

    Add legacy input state for native bridge

---
+++ LegacyIIiOS/Game/InputState.swift
@@ -0,0 +1,12 @@
+struct InputState {
+    var up = false
+    var down = false
+    var left = false
+    var right = false
+    var a = false
+    var b = false
+    var l = false
+    var r = false
+    var start = false
+    var select = false
+}
