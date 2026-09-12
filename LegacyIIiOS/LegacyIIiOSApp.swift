c61ed017137db9ccc6c717d8f0d7f60dc54a797c
seeynn <z3envx@web.de>
2026-09-12 08:14:51 +0200

    Add app entry point

---
+++ LegacyIIiOS/LegacyIIiOSApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct LegacyIIiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+                .statusBarHidden(true)
+        }
+    }
+}
