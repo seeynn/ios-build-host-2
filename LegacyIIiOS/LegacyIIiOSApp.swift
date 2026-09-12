41d94964504518817b50c7119267d29b9a979374
seeynn <z3envx@web.de>
2026-09-12 20:04:46 +0200

    Force true edge-to-edge iPhone presentation

--- LegacyIIiOS/LegacyIIiOSApp.swift
+++ LegacyIIiOS/LegacyIIiOSApp.swift
@@ -1,11 +1,40 @@
 import SwiftUI
+import UIKit
 
 @main
 struct LegacyIIiOSApp: App {
     var body: some Scene {
         WindowGroup {
-            RootView()
-                .statusBarHidden(true)
+            FullscreenRootController()
+                .ignoresSafeArea(.all)
         }
+    }
+}
+
+private struct FullscreenRootController: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        let controller = FullscreenHostingController(rootView: RootView())
+        controller.view.backgroundColor = .black
+        controller.modalPresentationCapturesStatusBarAppearance = true
+        return controller
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        uiViewController.setNeedsStatusBarAppearanceUpdate()
+        uiViewController.setNeedsUpdateOfHomeIndicatorAutoHidden()
+        uiViewController.setNeedsUpdateOfScreenEdgesDeferringSystemGestures()
+    }
+}
+
+private final class FullscreenHostingController<Content: View>: UIHostingController<Content> {
+    override var prefersStatusBarHidden: Bool { true }
+    override var prefersHomeIndicatorAutoHidden: Bool { true }
+    override var preferredScreenEdgesDeferringSystemGestures: UIRectEdge { .all }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setNeedsStatusBarAppearanceUpdate()
+        setNeedsUpdateOfHomeIndicatorAutoHidden()
+        setNeedsUpdateOfScreenEdgesDeferringSystemGestures()
     }
 }
